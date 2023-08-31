ac01428f5758526b46f2d19fe010a006440adc6b
no02 <no02@192.168.1.206>
2023-08-31 18:20:18 +0530

    Update script-MyAssignmentsPage

--- NowOnlineOne/src/main/java/com/nowonline/qa/pages/MyAssignmentsPage.java
+++ NowOnlineOne/src/main/java/com/nowonline/qa/pages/MyAssignmentsPage.java
@@ -24,7 +24,7 @@ public class MyAssignmentsPage extends TestBase{
 		@FindBy(xpath="//*[@id=\"initial-sort-select\"]/option[3]")
 		WebElement selectCompanyFromDropDown;
 		
-		@FindBy(xpath="//*[@id=\"table\"]/tbody/tr[2]/td[4]/div/a/span[2]")
+		@FindBy(xpath="//*[@id=\"table\"]/tbody/tr[1]/td[4]/div/a/span")
 		WebElement clickComments;
 		
 		@FindBy(xpath="//*[@id=\"my-companies\"]/div/div/div/div/table/tbody/tr[1]/td[7]/button[1]/span")
